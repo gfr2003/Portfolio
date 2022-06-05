634a04f39ab6e5c63c907068c3cb630e6a3bac6f
Gustavo Felix <gu2003fr@outlook.com>
2022-06-05 18:23:00 -0300

    feat(#16): carrosel

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-
+export class HomePage implements OnInit {
+  public slideIndex = 1;
   constructor() {}
+  ngOnInit() {
 
+    this.showSlides(this.slideIndex);
+  }
+  // Next/previous controls
+  public plusSlides(n) {
+    this.showSlides((this.slideIndex += n));
+  }
+
+  // Thumbnail image controls
+  public currentSlide(n) {
+    this.showSlides((this.slideIndex = n));
+  }
+
+  // eslint-disable-next-line @typescript-eslint/adjacent-overload-signatures
+  public showSlides(n) {
+    let i;
+    const slides = document.querySelectorAll<HTMLElement>('.mySlides');
+    const dots = document.getElementsByClassName('dot');
+    if (n > slides.length) {
+      this.slideIndex = 1;
+    }
+    if (n < 1) {
+      this.slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = 'none';
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(' active', '');
+    }
+    slides[this.slideIndex - 1].style.display = 'block';
+    dots[this.slideIndex - 1].className += ' active';
+  }
 }
