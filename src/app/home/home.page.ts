20d5a91a2ac25600bd14df7b8f5a70d2f9e255a1
Gustavo Felix <gu2003fr@outlook.com>
2022-06-26 16:03:16 -0300

    feat: general adjust

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,36 +10,36 @@ export class HomePage implements OnInit {
   constructor() {}
   ngOnInit() {
 
-    this.showSlides(this.slideIndex);
+    // this.showSlides(this.slideIndex);
   }
-  // Next/previous controls
-  public plusSlides(n) {
-    this.showSlides((this.slideIndex += n));
-  }
+  // // Next/previous controls
+  // public plusSlides(n) {
+  //   this.showSlides((this.slideIndex += n));
+  // }
 
-  // Thumbnail image controls
-  public currentSlide(n) {
-    this.showSlides((this.slideIndex = n));
-  }
+  // // Thumbnail image controls
+  // public currentSlide(n) {
+  //   this.showSlides((this.slideIndex = n));
+  // }
 
-  // eslint-disable-next-line @typescript-eslint/adjacent-overload-signatures
-  public showSlides(n) {
-    let i;
-    const slides = document.querySelectorAll<HTMLElement>('.mySlides');
-    const dots = document.getElementsByClassName('dot');
-    if (n > slides.length) {
-      this.slideIndex = 1;
-    }
-    if (n < 1) {
-      this.slideIndex = slides.length;
-    }
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = 'none';
-    }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(' active', '');
-    }
-    slides[this.slideIndex - 1].style.display = 'block';
-    dots[this.slideIndex - 1].className += ' active';
-  }
+  // // eslint-disable-next-line @typescript-eslint/adjacent-overload-signatures
+  // public showSlides(n) {
+  //   let i;
+  //   const slides = document.querySelectorAll<HTMLElement>('.mySlides');
+  //   const dots = document.getElementsByClassName('dot');
+  //   if (n > slides.length) {
+  //     this.slideIndex = 1;
+  //   }
+  //   if (n < 1) {
+  //     this.slideIndex = slides.length;
+  //   }
+  //   for (i = 0; i < slides.length; i++) {
+  //     slides[i].style.display = 'none';
+  //   }
+  //   for (i = 0; i < dots.length; i++) {
+  //     dots[i].className = dots[i].className.replace(' active', '');
+  //   }
+  //   slides[this.slideIndex - 1].style.display = 'block';
+  //   dots[this.slideIndex - 1].className += ' active';
+  // }
 }
