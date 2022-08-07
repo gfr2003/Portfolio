ee2fe609d5cebdcbf66f722456163ebf3a52c54e
Gustavo Felix <gu2003fr@outlook.com>
2022-08-07 17:04:33 -0300

    feature(#22): adjust home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,17 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit {
+export class HomePage implements OnInit, AfterViewInit {
   public slideIndex = 1;
   constructor() {}
+  ngAfterViewInit() {
+    const myContent = document.querySelector('#corpo');
+    this.styleScrollbars(myContent);
+  }
   ngOnInit() {
 
     // this.showSlides(this.slideIndex);
   }
+  styleScrollbars(elmt: any) {
+    const stylesheet = `
+    ::-webkit-scrollbar {
+      width: 9px;
+      background: #0f0f0f;
+    }
+    ::-webkit-scrollbar-thumb {
+      border-radius: 100px;
+      border: 5px solid transparent;
+      background-clip: content-box;
+      background-color: #8070d4;
+      background: #7456ff;
+    }
+    `;
+
+    const styleElmt = elmt.shadowRoot.querySelector('style');
+
+    if (styleElmt) {
+      styleElmt.append(stylesheet);
+    } else {
+      const barStyle = document.createElement('style');
+      barStyle.append(stylesheet);
+      elmt.shadowRoot.appendChild(barStyle);
+    }
+  }
+
+
+}
   // // Next/previous controls
   // public plusSlides(n) {
   //   this.showSlides((this.slideIndex += n));
@@ -42,4 +74,4 @@ export class HomePage implements OnInit {
   //   slides[this.slideIndex - 1].style.display = 'block';
   //   dots[this.slideIndex - 1].className += ' active';
   // }
-}
+
