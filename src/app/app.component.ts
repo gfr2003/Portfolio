e920838421a1bb1686469808f0e5da475dac1a0f
Gustavo Felix <gu2003fr@outlook.com>
2022-08-07 01:17:00 -0300

    feat(#19): add menu mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private menu: MenuController,private router: Router) {}
+  openMenu() {
+    this.menu.enable(true, 'first');
+    this.menu.open('first');
+  }
+  closeMenu(){
+    this.menu.close('first');
+  }
+  goTo(path: string){
+    this.router.navigateByUrl(path);
+    this.closeMenu();
+  }
 }
